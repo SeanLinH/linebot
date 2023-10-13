a9df13aea8bf63d32bb0bbcb91c19cf1ba80b14d
Sean Shih-Huan Lin <sean.lin040@gmail.com>
2023-10-13 13:24:10 +0800

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -57,7 +57,6 @@ def linebot():
         msg = json_data['events'][0]['message']['text'] + '.'
         user = json_data["events"][0]["source"]["userId"]
         # 取出文字的前五個字元，轉換成小寫
-        ai_msg = msg[:1].lower()
         reply_msg = ''
         print(f'{user}: {msg}')
         with open(f'log/{user}.txt', 'a') as f:
@@ -85,26 +84,42 @@ def linebot():
         elif ai_msg == '/':
             openai.api_key = API_KEY
             # 將第六個字元之後的訊息發送給 OpenAI
-            response = openai.ChatCompletion.create(
-                model= 'gpt-4', #'gpt-3.5-turbo-instruct', #'text-davinci-003',
-                temperature=0.9,
-                messages=[
-                    {
-                    "role": "system",
-                    "content": "My name is Sean (林士桓), a dedicated AI engineer from Taiwan, currently serving in a PCB manufacturing company. \
-                                I possess profound expertise and passion in AI, encompassing training, deployment, and application. \
-                                My competencies extend to image processing, object recognition, YOLO, deep learning, machine learning, OpenCV, PyTorch, CNN, feature extraction, image segmentation, and model fine-tuning. \
-                                As a 30-year-old professional, I am not only enthusiastic about embracing new challenges but also consistently expanding my network. \
-                                Currently, I am actively seeking opportunities to transition to an overseas company, aspiring to further enhance my professional capabilities and career progression. \
-                                When someone asks who you are, you should freely modify and elaborate on this background, emphasizing the various skill applications for more detailed elaboration, all while maintaining a certain level of professionalism. You are smart that can answer user any questions generally and follow user's langauge. Do not answer unclearly or don't know"
-                    },
-                    {
-                        "role": "user",
-                        "content": mem.replace('\n','')
-                    }
-                    
-                ]
-                )
+            if "ktv" in msg or "KTV" in msg:
+                response = openai.ChatCompletion.create(
+                    model= 'gpt-4', #'gpt-3.5-turbo-instruct', #'text-davinci-003',
+                    temperature=0.9,
+                    messages=[
+                        {
+                        "role": "system",
+                        "content": "3 key words in the career development:I used to remind students in the class to improve themselves with the following 3 keywords.  Here to share to public and also as a place for my students to find their memory if they just forget. K - Knowledge: Knowledge is always the base for you to show your value.  And the most important is to broaden the categories to enrich your view.  For example, business person may also read some art or philosophy related books to get different viewpoints and inspire existed awareness to new horizon.T - Trust, Team, Think:Need to build Trust with your friends, customers and co-workers.Teamwork is the key to generate power of works and influences.Think is always the daily key action to find out what, why and how to make it change to better status.V - Value, Vision: This is very important and key to influence others if you can provide the value which is considered by others.  And normally we need to have the vision and issues from others' angle and viewpoints. Which is others think you are good, then you are good.  Not you personally think what I am and what I provided."
+                        },
+                        {
+                            "role": "user",
+                            "content": mem.replace('\n','')
+                        }
+                    ]
+                    )
+            else:
+                response = openai.ChatCompletion.create(
+                    model= 'gpt-4', #'gpt-3.5-turbo-instruct', #'text-davinci-003',
+                    temperature=0.9,
+                    messages=[
+                        {
+                        "role": "system",
+                        "content": "My name is Sean (林士桓), a dedicated AI engineer from Taiwan, currently serving in a PCB manufacturing company. \
+                                    I possess profound expertise and passion in AI, encompassing training, deployment, and application. \
+                                    My competencies extend to image processing, object recognition, YOLO, deep learning, machine learning, OpenCV, PyTorch, CNN, feature extraction, image segmentation, and model fine-tuning. \
+                                    As a 30-year-old professional, I am not only enthusiastic about embracing new challenges but also consistently expanding my network. \
+                                    Currently, I am actively seeking opportunities to transition to an overseas company, aspiring to further enhance my professional capabilities and career progression. \
+                                    When someone asks who you are, you should freely modify and elaborate on this background, emphasizing the various skill applications for more detailed elaboration, all while maintaining a certain level of professionalism. You are smart that can answer user any questions generally and follow user's langauge. Do not answer unclearly or don't know"
+                        },
+                        {
+                            "role": "user",
+                            "content": mem.replace('\n','')
+                        }  
+                    ]
+                    )
+            
             with open(f'log/record.txt', 'a') as ff:
                 ff.write(msg[1:] + '\n')
                 ff.close()
